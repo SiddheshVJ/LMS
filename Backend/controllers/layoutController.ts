fe4643cf260e5d814a60aad6194f3456a264fe7a
siddhesh jadhav <siddheshvjadhav20@gmail.com>
2024-03-16 20:51:12 +0530

    All backend completed

--- Backend/controllers/layoutController.ts
+++ Backend/controllers/layoutController.ts
@@ -1,4 +1,4 @@
-import { Request, Response, NextFunction } from "express";
+import { Request, Response, NextFunction, response } from "express";
 import ErrorHandler from "../utils/errorHandler";
 import { catchAsyncError } from "../middleware/catchAsyncErrors";
 import layoutModel from "../models/layoutModel";
@@ -61,6 +61,96 @@ export const createLayout = catchAsyncError(
 			res.status(200).json({
 				success: true,
 				message: "Layout created successfully.",
+			});
+		} catch (error: any) {
+			return next(new ErrorHandler(error.message, 400));
+		}
+	}
+);
+
+// Edit Layout
+export const editLayout = catchAsyncError(
+	async (req: Request, res: Response, next: NextFunction) => {
+		try {
+			const { type } = req.body;
+
+			if (type === "Banner") {
+				const bannerData: any = await layoutModel.findOne({ type: "Banner" });
+				const { image, title, subTitle } = req.body;
+				if (bannerData) {
+					await cloudinary.v2.uploader.destroy(bannerData.image.public_id);
+				}
+
+				const myCloud = await cloudinary.v2.uploader.upload(image, {
+					folder: "Layouts",
+				});
+				const banner = {
+					image: {
+						public_id: myCloud.public_id,
+						url: myCloud.secure_url,
+					},
+					title,
+					subTitle,
+				};
+				await layoutModel.findByIdAndUpdate(bannerData._id, { banner });
+			}
+
+			if (type === "FAQ") {
+				const { faq } = req.body;
+
+				const faqItem = await layoutModel.findOne({ type: "FAQ" });
+
+				const faqItems = await Promise.all(
+					faq.map((item: any) => {
+						return {
+							question: item.question,
+							answer: item.answer,
+						};
+					})
+				);
+
+				await layoutModel.findByIdAndUpdate(faqItem._id, {
+					type: "FAQ",
+					faq: faqItems,
+				});
+			}
+
+			if (type === "Categories") {
+				const { categories } = req.body;
+				const categoriesData = await layoutModel.findOne({
+					type: "Categories",
+				});
+				const catItems = await Promise.all(
+					categories.map((item: any) => {
+						return {
+							title: item.title,
+						};
+					})
+				);
+				await layoutModel.findByIdAndUpdate(categoriesData._id, {
+					type: "Categories",
+					categories: catItems,
+				});
+			}
+
+			res.status(200).json({
+				success: true,
+				message: "Layout updated successfully.",
+			});
+		} catch (error) {
+			return next(new ErrorHandler(error.message, 400));
+		}
+	}
+);
+
+// get layout by  type
+export const getLayoutByType = catchAsyncError(
+	async (req: Request, res: Response, next: NextFunction) => {
+		try {
+			const layout = await layoutModel.findOne({ type: req.body.type });
+			res.status(200).json({
+				success: true,
+				layout,
 			});
 		} catch (error: any) {
 			return next(new ErrorHandler(error.message, 400));
--- Backend/routes/layoutRoute.ts
+++ Backend/routes/layoutRoute.ts
@@ -1,6 +1,10 @@
 import express from "express";
 import { authorisedRoles, isAuthenticated } from "../middleware/auth";
-import { createLayout } from "../controllers/layoutController";
+import {
+	createLayout,
+	editLayout,
+	getLayoutByType,
+} from "../controllers/layoutController";
 
 const layoutRoute = express.Router();
 
@@ -9,6 +13,18 @@ layoutRoute.post(
 	isAuthenticated,
 	authorisedRoles("admin"),
 	createLayout
+);
+layoutRoute.put(
+	"/update-layout",
+	isAuthenticated,
+	authorisedRoles("admin"),
+	editLayout
+);
+layoutRoute.get(
+	"/get-layout",
+	isAuthenticated,
+	authorisedRoles("admin"),
+	getLayoutByType
 );
 
 export default layoutRoute;
